7ff70a6377283f6cc2dcaab8ba369a4700ab5a56
Babbaj <babbaj45@gmail.com>
2022-12-09 11:28:22 -0500

    day 9: unwashed part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 type Pos = (i32, i32);
 type Move = (char, u8);
-#[derive(Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 enum Dir {
     U,
     D,
@@ -22,7 +22,7 @@ impl Dir {
 }
 
 
-fn main() {
+fn part1() {
     let input = include_str!("../input");
     let moves = input.lines()
         .map(|l| l.split_once(' ').unwrap())
@@ -35,18 +35,47 @@ fn main() {
     for (dir, dist) in moves {
         for _ in 0..dist {
             let new_head = move_head(dir, head);
-            match move_to_head(new_head, tail) {
-                Some(new_tail) => {
-                    tail_visits.insert(new_tail);
-                    tail = new_tail;
-                },
-                None => {}
+            if let Some(new_tail) =  move_to_head(new_head, tail) {
+                tail_visits.insert(new_tail);
+                tail = new_tail;
             }
             head = new_head;
         }
     }
 
     println!("part 1 = {}", tail_visits.len());
+}
+
+fn main() {
+    let input = include_str!("../input");
+    let moves = input.lines()
+        .map(|l| l.split_once(' ').unwrap())
+        .map(|(a, num)| (Dir::parse(a), num.parse::<u8>().unwrap()));
+    let mut knots = [(0, 0); 10];
+    let mut tail_visits = HashSet::<(i32, i32)>::new();
+    tail_visits.insert((0, 0));
+
+    for (dir, dist) in moves {
+        println!("dir = {:?}, dist = {}", dir, dist);
+        for n in 0..dist {
+            knots[0] = move_head(dir, knots[0]);
+            println!("head = {:?}", knots[0]);
+            if let Some(tail1) = move_to_head(knots[0], knots[1]) {
+                knots[1] = tail1;
+            }
+            for i in 2..=9 {
+                let h = knots[i - 1];
+                let t = knots[i];
+                if let Some(new_tail) =  tail_follow_tail(h, t) {
+                    knots[i] = new_tail;
+                }
+            }
+            tail_visits.insert(knots[9]);
+            println!("tail = {:?}", knots[9]);
+        }
+    }
+
+    println!("part 2 = {}", tail_visits.len());
 }
 
 fn move_head(dir: Dir, (x, y): Pos) -> Pos {
@@ -74,7 +103,7 @@ fn adjacent(h: Pos, t: Pos) -> bool {
 
 fn move_to_head(head: Pos, (tx, ty): Pos) -> Option<Pos> {
     if touching(head, (tx, ty)) {
-        return None // touching
+        return None // already touching, no new move
     }
 
     let mut new_tails = [
@@ -89,6 +118,30 @@ fn move_to_head(head: Pos, (tx, ty): Pos) -> Option<Pos> {
     ].iter()
         .map(|(dx, dy)| (tx + dx, ty + dy))
         .filter(|new_tail| adjacent(head, *new_tail));
+
+    new_tails.next()
+}
+
+fn tail_follow_tail(head: Pos, (tx, ty): Pos) -> Option<Pos> {
+    if touching(head, (tx, ty)) {
+        return None // already touching, no new move
+    }
+
+    if let Some(new_tail) = move_to_head(head, (tx, ty)) {
+        return Some(new_tail)
+    }
+    let mut new_tails = [
+        (-1, 1), // up left
+        (0, 1), // up
+        (1, 1), // up right
+        (-1, 0), // left
+        (1, 0), // right
+        (-1, -1), // down left
+        (0, -1), // down
+        (1, -1) // down right
+    ].iter()
+        .map(|(dx, dy)| (tx + dx, ty + dy))
+        .filter(|new_tail| touching(head, *new_tail));
 
     let new_tail = new_tails.next().unwrap();
     assert_eq!(new_tails.next(), None);
