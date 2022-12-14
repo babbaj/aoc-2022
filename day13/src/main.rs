e5a39ca70a805da6e86acb2e24b3d1ec1da11fcb
Babbaj <babbaj45@gmail.com>
2022-12-14 16:53:49 -0500

    day 13: sorting isn't necessary

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -42,7 +42,7 @@ fn compare_slice(left: &[Packet], right: &[Packet]) -> Ordering {
 
 fn compare(first: &Packet, second: &Packet) -> Ordering {
     match (first, second) {
-        (Num(n1), Num(n2)) => n1.cmp(n2),
+        (Num(left), Num(right)) => left.cmp(right),
         (List(left), List(right)) => compare_slice(left, right),
         (List(left), Num(right)) => compare_slice(left, &[Num(*right)]),
         (Num(left), List(right)) => compare_slice(&[Num(*left)], right)
@@ -52,7 +52,7 @@ fn compare(first: &Packet, second: &Packet) -> Ordering {
 fn main() {
     let input = include_str!("../input");
 
-    let mut packets = input.split('\n')
+    let packets = input.lines()
         .filter(|line| !line.is_empty())
         .map(|line| parse_packet(line).unwrap().1)
         .collect::<Vec<_>>();
@@ -60,7 +60,7 @@ fn main() {
     let pairs = (0..packets.len()).step_by(2)
         .map(|i| (&packets[i], &packets[i + 1]));
     let part1 = pairs.enumerate()
-        .map(|(i, (a, b))| (i, compare(&a, &b)))
+        .map(|(i, (a, b))| (i, compare(a, b)))
         .filter(|(_, ord)| *ord != Ordering::Greater)
         .map(|(i, _)| i + 1)
         .sum::<usize>();
@@ -68,13 +68,15 @@ fn main() {
     let dividers = [2, 6].map(|n|
         List(vec![List(vec![Num(n)])])
     );
-    packets.extend_from_slice(&dividers);
-    packets.sort_by(compare);
 
-    let part2: usize = packets.iter().enumerate()
-        .filter(|(_, packet)| dividers.contains(packet))
-        .map(|(i, _)| i + 1)
-        .product();
+    let part2: i32 = packets.iter()
+        .map(|packet| [compare(&dividers[0], packet), compare(&dividers[1], packet)])
+        .map(|ord| ord.map(|o| match o {
+            Ordering::Greater => 1,
+            _ => 0
+        }))
+        .fold([1, 2], |acc, ord| [acc[0] + ord[0], acc[1] + ord[1]])
+        .iter().product();
 
     println!("part 1 = {}, part 2 = {}", part1, part2);
 }
